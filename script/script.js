cd026187c8105e107458683a665822bb9959854b
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-10 15:29:09 +0100

    logic js

---
+++ script/script.js
@@ -0,0 +1,17 @@
+// Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+// Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
+
+// richiamo il bottone
+const bottone = document.querySelector('button');
+// richiamo l'immagine
+const immagine = document.querySelector('img');
+
+document.querySelector('.light-off').url="./img/yellow-lamp"
+
+
+// Aggiungo un evento al bottone per cambiare l'immagine al clic
+
+
+// Cambia la sorgente dell'immagine con quella nuova
+
+
